bacbf5c5a006fdc6b241587a2e18f51d7413ba25
lyw <2354640364@qq.com>
2020-01-29 18:45:27 +0800

    类的继承

--- test2/class.js
+++ test2/class.js
@@ -1,4 +1,17 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 //class类（属性、方法）
 var Person = /** @class */ (function () {
     function Person(name, username) {
@@ -19,6 +32,23 @@ var Person = /** @class */ (function () {
     return Person;
 }());
 var person = new Person('even', 'lyw');
-console.log(person.name, person.username);
-person.printAge(21);
-person.setGender('女');
+// console.log(person.name, person.username)
+// person.printAge(21)
+// person.setGender('女')
+//Student 类 继承于 person类
+var Student = /** @class */ (function (_super) {
+    __extends(Student, _super);
+    function Student(name, username, studentId) {
+        var _this = _super.call(this, name, username) || this;
+        _this.studentId = studentId;
+        return _this;
+    }
+    Student.prototype.studentPrint = function () {
+        console.log(this.studentId);
+    };
+    return Student;
+}(Person));
+var lyw = new Student('dudu', '嘟嘟', 610911);
+console.log(lyw);
+lyw.studentPrint();
+lyw.printAge(21);
--- test2/class.ts
+++ test2/class.ts
@@ -21,7 +21,26 @@ class Person {
 }
 
 const person = new Person('even', 'lyw')
-console.log(person.name, person.username)
+// console.log(person.name, person.username)
 
-person.printAge(21)
-person.setGender('女')
+// person.printAge(21)
+// person.setGender('女')
+
+//Student 类 继承于 person类
+class Student extends Person {
+  studentId: number
+  constructor(name: string, username: string, studentId: number) {
+    super(name, username)
+    this.studentId = studentId
+  }
+
+  studentPrint() {
+    console.log(this.studentId)
+  }
+}
+
+const lyw = new Student('dudu', '嘟嘟', 610911)
+console.log(lyw)
+
+lyw.studentPrint()
+lyw.printAge(21)
