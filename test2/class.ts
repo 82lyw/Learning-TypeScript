26a2d4f7da3605771f8e0a6c3062ca525ddc3b0e
lyw <2354640364@qq.com>
2020-01-29 14:12:18 +0800

    class类（属性、方法）

---
+++ test2/class.ts
@@ -0,0 +1,27 @@
+//class类（属性、方法）
+class Person {
+  public name: string
+  protected gender: string = '男'
+  private age: number = 27
+
+  constructor(name: string, public username: string) {
+    this.name = name
+    this.username = username
+  }
+
+  printAge(age: number) {
+    this.age = age
+    console.log(this.age)
+  }
+
+  setGender(gender: string) {
+    this.gender = gender
+    console.log(this.gender)
+  }
+}
+
+const person = new Person('even', 'lyw')
+console.log(person.name, person.username)
+
+person.printAge(21)
+person.setGender('女')
